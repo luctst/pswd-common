cbbb5ea123f1a626d2a4c5b62a585f6dcf18df36
luctst <lucas.tostee@gmail.com>
2021-09-04 13:29:10 +0200

    Start adding back macros

--- macros/index.js
+++ macros/index.js
@@ -1,1 +1,18 @@
-module.exports = {};
+module.exports = {
+    back: {
+        users: {
+            projection: {
+                password: 0,
+                __v: 0
+            }
+        },
+        mongoose: {
+            fieldsFilters: ['sort', 'skip', 'limit', 'order']
+        },
+        sessions: {
+            populate: [
+                { path: 'userId', select: 'name familyName mail' }
+            ]
+        }
+    },
+};
