6354f4f3315af07910fdc1956643e49ba069405d
luctst <lucas.tostee@gmail.com>
2021-09-07 17:56:24 +0200

    Update user model

--- models/users.js
+++ models/users.js
@@ -11,14 +11,32 @@ const UsersSchema = new Schema({
     },
     mail: {
         type: String,
-        required: true,
+        required() {
+            if (this.isRootAdmin) return true;
+            return false;
+        },
         unique: true,
         match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        minlength: 8
     },
+    isRootAdmin: {
+        type: Boolean,
+        default() {
+            if (this.mail) return true;
+            return false;
+        }
+    },
+    teamId: {
+        type: Schema.Types.ObjectId,
+        required() {
+            if (!this.isRootAdmin) return true;
+            return false;
+        }
+    }
 });
 
 module.exports = UsersSchema
