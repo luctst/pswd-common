beff5c790222f359d6f4eb5be514d93ff07e1691
luctst <lucas.tostee@gmail.com>
2021-09-22 17:21:01 +0200

    Add teams model

--- models/index.js
+++ models/index.js
@@ -2,8 +2,10 @@ const { model } = require('mongoose');
 
 const sessionsSchema = require('./sessions');
 const usersSchema = require('./users')
+const teamsSchema = require('./teams');
 
 module.exports = {
     Sessions: model('Sessions', sessionsSchema),
     Users: model('Users', usersSchema),
+    Teams: model('Teams', teamsSchema)
 }
