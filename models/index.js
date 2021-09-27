3559084fa947c306e2f946698223b7564f3eaa54
luctst <lucas.tostee@gmail.com>
2021-09-27 17:23:00 +0200

    Add cards model

--- models/index.js
+++ models/index.js
@@ -3,9 +3,11 @@ const { model } = require('mongoose');
 const sessionsSchema = require('./sessions');
 const usersSchema = require('./users')
 const teamsSchema = require('./teams');
+const cardsSchema = require('./cards');
 
 module.exports = {
     Sessions: model('Sessions', sessionsSchema),
     Users: model('Users', usersSchema),
-    Teams: model('Teams', teamsSchema)
+    Teams: model('Teams', teamsSchema),
+    Cards: model('Cards', cardsSchema),
 }
