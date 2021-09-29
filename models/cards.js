01441b5cb4eadab93b672f8a2d7da78fc215e93e
luctst <lucas.tostee@gmail.com>
2021-09-29 18:11:35 +0200

    Start create cards schema

--- models/cards.js
+++ models/cards.js
@@ -2,10 +2,29 @@ const { Schema } = require('mongoose');
 
 const CardsSchema = new Schema(
     {
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: 'Users',
+            required() {
+                return this.teamId ? false : true;
+            }
+        },
         name: {
             type: String,
             required: true,
             maxlength: 30,
+        },
+        data: {
+            required: true,
+            type: Map,
+            of: String,
+        },
+        teamId: {
+            type: Schema.Types.ObjectId,
+            ref: 'Teams',
+            required() {
+                return this.author ? false : true;
+            },
         },
     },
     {
